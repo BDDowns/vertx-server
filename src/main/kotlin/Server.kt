8094de7757f6fb79a5c391356b873586fb98d721
Bryan Downs <BDDowns@users.noreply.github.com>
2023-03-04 06:00:01 -0700

    add json response

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -1,13 +1,20 @@
 import io.vertx.core.Vertx
 import io.vertx.ext.web.Router
+import io.vertx.kotlin.core.json.json
+import io.vertx.kotlin.core.json.obj
 
 fun main(args: Array<String>) {
     val vertx = Vertx.vertx()
     val router = Router.router(vertx)
+    val json = json {
+        obj(
+            "status" to "OK"
+        )
+    }
     router.get("/status").handler {
         it.response()
             .setStatusCode(200)
-            .end("OK")
+            .end(json.toString())
     }
     vertx.createHttpServer()
         .requestHandler(router)
