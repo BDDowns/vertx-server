dd0caa42eb63bb926e1facb79aad9a16171bc341
Bryan Downs <BDDowns@users.noreply.github.com>
2023-03-05 05:29:37 -0700

    add subrouter

--- src/main/kotlin/ServerVerticle.kt
+++ src/main/kotlin/ServerVerticle.kt
@@ -1,3 +1,4 @@
+import io.vertx.core.json.JsonObject
 import io.vertx.ext.web.Router
 import io.vertx.kotlin.core.json.json
 import io.vertx.kotlin.core.json.obj
@@ -5,26 +6,35 @@ import io.vertx.kotlin.coroutines.CoroutineVerticle
 
 class ServerVerticle : CoroutineVerticle() {
     override suspend fun start() {
-        val router = Router.router(vertx)
-        val json = json {
+        val mainRouter = Router.router(vertx)
+        mainRouter.get("/status").handler {
+            it.response()
+                .setStatusCode(200)
+                .end(getHealth().toString())
+        }
+        mainRouter.route().subRouter(usersRouter())
+        vertx.createHttpServer()
+            .requestHandler(mainRouter)
+            .listen(8081)
+        println("open http://localhost:8081/health")
+    }
+
+    private fun getHealth(): JsonObject {
+        return json {
             obj(
                 "status" to "OK"
             )
         }
-        router.get("/status").handler {
-            it.response()
-                .setStatusCode(200)
-                .end(json.toString())
-        }
+    }
+
+    private fun usersRouter(): Router {
+        val router = Router.router(vertx)
         router.delete("/users/:id").handler {
             TODO("code for deleting users")
         }
         router.put("/users/:id").handler {
             TODO("code for updating a user")
         }
-        vertx.createHttpServer()
-            .requestHandler(router)
-            .listen(8081)
-        println("open http://localhost:8081/status")
+        return router
     }
 }
